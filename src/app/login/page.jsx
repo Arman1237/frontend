634c8fc7781421b5891584e197d7078143bad741
Arman1237 <nasirkhan780022@gmail.com>
2024-10-07 17:27:42 +0530

    React page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h2 className ="text-5x1 text-center">Login Page</h2>
+        
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className="text-4x1 font-bold text-center">Hello React</h1>
+    </div>
+  )
+}
+
+export default Home;
