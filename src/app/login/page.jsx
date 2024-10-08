83a70d7326aa05325982cfb61a68f78ed06db9aa
Arman1237 <nasirkhan780022@gmail.com>
2024-10-08 17:42:09 +0530

    React

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import classes from './login.module.css';
 
 const Login = () => {
   return (
     <div>
         <h2 className ="text-5x1 text-center">Login Page</h2>
-        
+
+       <button className="submit-btn">Login Button</button> 
+
+       <button className={classes.btn}>another button</button>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,18 @@ const Home = () => {
   return (
     <div>
         <h1 className="text-4x1 font-bold text-center">Hello React</h1>
+        <button style={{backgroundcolor : 'black', color: 'white', padding: 10}}>
+          Home Button
+          </button>
+          <br />
+          <input type="text" />
+
+          <button className="submit-btn">Home button</button>
+          <button className="btn">another button</button>
+
+          <img src="/minions.avif.jpeg" alt="" />
+
+
     </div>
   )
 }
